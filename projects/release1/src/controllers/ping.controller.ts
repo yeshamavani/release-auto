494ab4e6620369cdad70695cffde7192e3bfa447
Yesha  Mavani <yesha.mavani@sourcefuse.com>
2021-10-12 11:36:33 +0530

    with git token

--- projects/release1/src/controllers/ping.controller.ts
+++ projects/release1/src/controllers/ping.controller.ts
@@ -46,7 +46,7 @@ export class PingController {
   ping(): object {
     // Reply with a greeting, the current time, the url, and request headers
     return {
-      greeting: 'Hello from LoopBack5',
+      greeting: 'Hello from LoopBack6',
       date: new Date(),
       url: this.req.url,
       headers: Object.assign({}, this.req.headers),
