728738b0218c92e8fe3278251c036b435e098486
yesha.mavani <yesha.mavani@sourcefuse.com>
2023-04-13 12:36:15 +0530

    feat(chore): new tag
    gh-00

--- projects/release1/src/controllers/ping.controller.ts
+++ projects/release1/src/controllers/ping.controller.ts
@@ -46,7 +46,7 @@ export class PingController {
   ping(): object {
     // Reply with a greeting, the current time, the url, and request headers
     return {
-      greeting: 'Hello from LoopppBack1',
+      greeting: 'Hello from LoopppBack12',
       date: new Date(),
       url: this.req.url,
       headers: Object.assign({}, this.req.headers),
