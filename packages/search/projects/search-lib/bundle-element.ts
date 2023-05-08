111bb898478667dedfc9ff007a34a51302dfb1d7
yeshamavani <yesha.mavani@sourcefuse.com>
2023-05-08 22:21:05 +0530

    feat(chore): yy
    gh-22

--- packages/search/projects/search-lib/bundle-element.ts
+++ packages/search/projects/search-lib/bundle-element.ts
@@ -1,22 +1,15 @@
-import {ensureDir, readFileSync, writeFileSync, copySync} from 'fs-extra';
+import {ensureDir, readFileSync, writeFileSync} from 'fs-extra';
 import {copyFile, rm} from 'fs/promises';
+import * as crypto from 'crypto';
 const concat = require('concat');
 
 async function elementsBundler() {
-  //const space = 2;
+  const space = 2;
   const files = [
     './dist/search-element/runtime.js',
     './dist/search-element/polyfills.js',
     './dist/search-element/main.js',
   ];
-  // await ensureDir('./dist/element');
-  // await concat(files, './dist/element/search-element.js');
-  // await copyFile(
-  //   './dist/search-element/styles.css',
-  //   './dist/element/styles.css',
-  // );
-  // await rm('./dist/search-element', {recursive: true});
-  // copySync('./dist/element', '../search-element/element');
 
   await ensureDir('../search-element/dist');
   await concat(files, '../search-element/dist/search-element.js');
@@ -27,20 +20,29 @@ async function elementsBundler() {
   await copyFile('./dist/README.md', '../search-element/dist/README.md');
   await rm('./dist/search-element', {recursive: true});
 
-  //copy the package.json and change the name to
-  //so that element can be released separately with different name
-  //the versions will be same
-  // const packageJsonPath = './package.json';
-  // const jsonObj = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
-  // jsonObj.name = 'search-element-dont-use';
-  // delete jsonObj.peerDependencies;
-  // writeFileSync(
-  //   './dist/element/package.json',
-  //   JSON.stringify(jsonObj, null, space),
-  // );
+  // generate the hash of element file to keep a track of the chnages
+  // whenever any changes are made in the element the corresponding js file will
+  // resulting in changing its hash, so the element will also be published
 
-  /// copy the entire element to another folder
-  // as we cannot release it from same dist folder
+  const file = '../search-element/dist/search-element.js';
+  try {
+    const data = readFileSync(file);
+    const hash = crypto.createHash('sha256');
+    let fileHash = '';
+    hash.update(data);
+
+    fileHash = hash.digest('hex');
+    console.log(`The SHA-256 hash of ${file} is: ${fileHash}`);
+
+    //update the package.json with the new hashCode
+    //if there will be changes in hash then the element will also be published
+    const packageJsonPath = '../search-element/package.json';
+    const jsonObj = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
+    jsonObj.hash = fileHash;
+    writeFileSync(packageJsonPath, JSON.stringify(jsonObj, null, space));
+  } catch (err: any) {
+    console.error(`An error occurred while reading ${file}: ${err.message}`);
+  }
 }
 
 elementsBundler();
